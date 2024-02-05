70f3e7f287e758a592229aa80d5128a5fda2aa28
javvadanappalam <115083225+javvadanappalam@users.noreply.github.com>
2024-02-05 12:12:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -49,7 +49,7 @@ function loadShops(shopType,under) {
         shops.forEach(shop => {
             shopItem.id = 'shoplist';
             const li = document.createElement('li');
-            li.innerHTML = `<a href="#" onclick="showShopDetails('${shop.name}', '${shop.location}','$(shop.imagePath)')">${shop.name}</a>`;
+            li.innerHTML = `<a href="#" onclick="showShopDetails('${shop.name}', '${shop.location}','${shop.imagePath}')">${shop.name}</a>`;
             li.style = "list-style:none;background-color: white;padding: 20px;border-radius: 10px;border: 1px solid black;"
             ul.appendChild(li)
             shopItem.appendChild(ul)
