1229bda9a00a4f4eae5ba98c0b42b915fc0b5666
javvadanappalam <115083225+javvadanappalam@users.noreply.github.com>
2024-02-05 12:07:53 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const shopData = {
     grocery: {
      supermarkets:[
-        { name: 'MKH supermarket', location: 'palakkad,thrippanachi',imagePath:'https://images.app.goo.gl/3cEsiizfgugjATee8'},
+        { name: 'MKH supermarket', location: 'palakkad,thrippanachi',imagePath:'mkh.jpg'},
         { name: 'Rayan supermarket', location: 'pookolathur',imagePath: 'https://images.app.goo.gl/nq2qhpbqhvn7hBjE8'},
     ],
     convenience:[
